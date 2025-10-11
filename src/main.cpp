7dcda1a76fb3df1ceecb99f53651e69818f1ea91
rarfel <faelmvitor@gmail.com>
2025-10-11 13:28:00 -0300

    Different kinds of triangles

--- src/main.cpp
+++ src/main.cpp
@@ -5,28 +5,23 @@
 
 int main(void)
 {
-    int i = 0;
-    bool turning = false;
-    InitWindow(WIDTH, HEIGHT, "Creating a line first");
+    InitWindow(WIDTH, HEIGHT, "Creating a triangle");
+
+    Vector2 v1 = {100,100};
+    Vector2 v2 = {100,200};
+    Vector2 v3 = {150,150};
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
             ClearBackground(BLACK);
-            DrawLine(i,0,WIDTH,HEIGHT,WHITE);
-            DrawLine(i,HEIGHT,WIDTH,0,WHITE);
-            if(turning)
-            {
-                i--;
-                if(i < 0)
-                    turning = false;
-            }
-            else if(!turning)
-            {
-                i++;
-                if(i >= WIDTH)
-                    turning = true;
-            }
+            //the DrawTriangle function creates a solid triangle
+            // DrawTriangle(v1, v2, v3, WHITE);
+
+            //If i want only the outline a have to use DrawLine 3 times
+            DrawLine(v1.x, v1.y, v2.x, v2.y, WHITE);
+            DrawLine(v2.x, v2.y, v3.x, v3.y, WHITE);
+            DrawLine(v3.x, v3.y, v1.x, v1.y, WHITE);
         EndDrawing();
     }
 
