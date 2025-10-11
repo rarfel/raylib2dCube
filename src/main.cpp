8a4ae160cea1545b188870865d5c86653af47185
rarfel <faelmvitor@gmail.com>
2025-10-11 13:19:55 -0300

    loop iteractions and instant changes test

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,28 @@
 
 int main(void)
 {
+    int i = 0;
+    bool turning = false;
     InitWindow(WIDTH, HEIGHT, "Creating a line first");
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
             ClearBackground(BLACK);
-            DrawLine(0,0,WIDTH,HEIGHT,WHITE);
-            DrawLine(0,HEIGHT,WIDTH,0,WHITE);
+            DrawLine(i,0,WIDTH,HEIGHT,WHITE);
+            DrawLine(i,HEIGHT,WIDTH,0,WHITE);
+            if(turning)
+            {
+                i--;
+                if(i < 0)
+                    turning = false;
+            }
+            else if(!turning)
+            {
+                i++;
+                if(i >= WIDTH)
+                    turning = true;
+            }
         EndDrawing();
     }
 
