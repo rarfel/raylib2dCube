7e3d324dff794b61aeaccf7a5a4430262013375e
rarfel <faelmvitor@gmail.com>
2025-10-12 00:16:03 -0300

    laying down the basics, implement a full cube later

--- headers/logic.h
+++ headers/logic.h
@@ -1,1 +1,81 @@
-//future functions will be here to use
+#include <iostream>
+#include <cmath>
+
+// dx = cosy(sinzY + coszX) - sinyZ
+float RotateXAxis(Vector3 vec, float angleBeta, float angleGamma)
+{
+    float sinBeta = std::sin(angleBeta);
+    float sinGamma = std::sin(angleGamma);
+
+    float cosBeta = std::cos(angleBeta);
+    float cosGamma = std::cos(angleGamma);
+
+    return cosBeta * (sinGamma * vec.y + cosGamma * vec.x) - sinBeta * vec.z;
+}
+
+// dy = sinx(cosyZ + siny(sinzY + coszX)) + cosx(coszY - sinzX)
+float RotateYAxis(Vector3 vec, float angleAlpha, float angleBeta, float angleGamma)
+{
+    float sinAlpha = std::sin(angleAlpha);
+    float sinBeta = std::sin(angleBeta);
+    float sinGamma = std::sin(angleGamma);
+
+    float cosAlpha = std::cos(angleAlpha);
+    float cosBeta = std::cos(angleBeta);
+    float cosGamma = std::cos(angleGamma);
+
+    return sinAlpha * (cosBeta * vec.z + sinBeta * (sinGamma * vec.y + cosGamma * vec.x)) + cosAlpha * (cosGamma * vec.y - sinGamma * vec.x);
+}
+
+// dz = cosx(cosyZ + siny(sinzY + coszX)) - sinx(coszY - sinzX)
+float RotateZAxis(Vector3 vec, float angleAlpha, float angleBeta, float angleGamma)
+{
+    float sinAlpha = std::sin(angleAlpha);
+    float sinBeta = std::sin(angleBeta);
+    float sinGamma = std::sin(angleGamma);
+
+    float cosAlpha = std::cos(angleAlpha);
+    float cosBeta = std::cos(angleBeta);
+    float cosGamma = std::cos(angleGamma);
+
+    return cosAlpha * (cosBeta * vec.z + sinBeta * (sinGamma * vec.y + cosGamma * vec.x)) - sinAlpha * (cosGamma * vec.y - sinGamma * vec.x);
+}
+
+// Rotates the points in all axis
+Vector3 RotateXYZAxis(Vector3 vec, float angleAlpha, float angleBeta, float angleGamma)
+{
+    Vector3 nVec;
+
+    nVec.x = RotateXAxis(vec,angleBeta,angleGamma);
+    nVec.y = RotateYAxis(vec,angleAlpha,angleBeta,angleGamma);
+    nVec.z = RotateZAxis(vec,angleAlpha,angleBeta,angleGamma);
+
+    return nVec;
+}
+
+// projected = (x or y) * fov / z + fov
+Vector3 ProjectedCoordinates(Vector3 vec, float fov)
+{
+    Vector3 projected;
+
+    projected.x = (vec.x * fov) / (vec.z + fov);
+    projected.y = (vec.y * fov) / (vec.z + fov);
+    projected.z = vec.z;
+
+    return projected;
+}
+
+// function that draws a tringle with tiny circles on the edges
+void DrawTriangleHollow(Vector3 v1, Vector3 v2, Vector3 v3, bool withCircles, Color color)
+{
+    if(withCircles)
+    {
+        int circleSize = 5;
+        DrawCircle(v1.x, v1.y, circleSize, color);
+        DrawCircle(v2.x, v2.y, circleSize, color);
+        DrawCircle(v3.x, v3.y, circleSize, color);
+    }
+    DrawLine(v1.x, v1.y, v2.x, v2.y, color);
+    DrawLine(v2.x, v2.y, v3.x, v3.y, color);
+    DrawLine(v3.x, v3.y, v1.x, v1.y, color);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,56 @@
 
 int main(void)
 {
-    const int squareSize = 1;
-    InitWindow(WIDTH, HEIGHT, "Creating colored grid");
+    InitWindow(WIDTH, HEIGHT, "3d Objects");
+
+    float x1 = 200;
+    float y1 = 200;
+    float z1 = 1;
+
+    float x2 = 400;
+    float y2 = 400;
+    float z2 = 1;
+
+    float x3 = 400;
+    float y3 = 200;
+    float z3 = 1;
+    
+    Vector3 v1 = {x1,y1,z1};
+    Vector3 v2 = {x2,y2,z2};
+    Vector3 v3 = {x3,y3,z3};
+    
+    Vector3 nv1;
+    Vector3 nv2;
+    Vector3 nv3;
+
+    int fov = 100;
+
+    float angleAlpha = 0;
+    float angleBeta = 0;
+    float angleGamma = 0;
 
     while (!WindowShouldClose())
     {
+        if(IsKeyDown(KEY_ONE)) {angleAlpha += 0.005;}
+        if(IsKeyDown(KEY_TWO)) {angleBeta += 0.005;}
+        if(IsKeyDown(KEY_THREE)) {angleGamma += 0.005;}
+
+        if(IsKeyDown(KEY_FOUR)) {angleAlpha -= 0.005;}
+        if(IsKeyDown(KEY_FIVE)) {angleBeta -= 0.005;}
+        if(IsKeyDown(KEY_SIX)) {angleGamma -= 0.005;}
+
         BeginDrawing();
+        
+        nv1 = RotateXYZAxis({x1,y1,z1},angleAlpha,angleBeta,angleGamma);
+        nv2 = RotateXYZAxis({x2,y2,z2},angleAlpha,angleBeta,angleGamma);
+        nv3 = RotateXYZAxis({x3,y3,z3},angleAlpha,angleBeta,angleGamma);
+
+        v1 = ProjectedCoordinates(nv1,fov);
+        v2 = ProjectedCoordinates(nv2,fov);
+        v3 = ProjectedCoordinates(nv3,fov);
+
         ClearBackground(BLACK);
-        for(int i = 0; i < WIDTH; i++)
-            for(int j = 0; j < HEIGHT; j++)
-                //The rgba values are stored in unsigned char, so converting is a must
-                DrawRectangle(i*squareSize, j*squareSize, squareSize, squareSize, {(unsigned char)(int((i*squareSize)/(3.85))%255), (unsigned char)(int((j*squareSize)/(2.85))%255), 0, 255});            
+        DrawTriangleHollow(v1, v2, v3, true, PURPLE);
         EndDrawing();
     }
 
